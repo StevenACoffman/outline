e40cb60e086a86bea456898e197f17812b34b465
Steve Coffman <gears@umich.edu>
2016-09-24 22:55:25 -0400

    Add bootstrap and see what is going on

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {FormGroup, FormControl, Button} from 'react-bootstrap';
 import './App.css';
+
+const styles = {
+    output: {height: "21.5em"},
+    outputRender: {height: "100%"}
+};
 
 class App extends Component {
   render() {
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      <FormGroup>
+                    <FormControl componentClass="textarea" rows="6" id="codeInput" />
+                </FormGroup>
+                <FormGroup>
+                    <Button block>Run!</Button>
+                </FormGroup>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Button from 'react-bootstrap/lib/Button';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App />, <Button bsStyle='success' bsSize='large'>Get started today</Button>);
 });
